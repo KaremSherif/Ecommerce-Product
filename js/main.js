a04ab58001d17dea2ca097aca371947331ec53d9
karemSherif <karemsherif922@gmail.com>
2022-06-14 23:56:13 +0200

    Done For Mobiles

--- js/main.js
+++ js/main.js
@@ -1,13 +1,25 @@
 const humburger = document.querySelector('.humburger')
 const menu = document.querySelector('.menu')
+const card = document.querySelector('.cart-popUp')
+const btnCard = document.querySelector('.btnCard')
 
+// remove hidden menu by click at humburger img
 humburger.addEventListener('click', removeHidden)
-menu.addEventListener('click', addHidden)
 
 function removeHidden(){
     menu.classList.remove('hidden')
 }
+// add hidden while closeing menu
+menu.addEventListener('click', addHidden)
+
 function addHidden(){
     menu.classList.add('hidden')
 }
+// popup orders whille click at card numbers to show prices
+btnCard.addEventListener('click', popCart)
+
+function popCart(){
+    card.classList.toggle('hidden')
+}
+
 
